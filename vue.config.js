cf30f78ce4de493faefe80144358ed9299d0c815
cgrountree <cgrountree@gmail.com>
2021-03-18 16:35:57 -0400

    stop infinite loop

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,4 @@
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production' ? '/cgrountree.github.io/' : '/',
-    css: {
-        loaderOptions: {
-            sass: { // for sass-loader < 8.0.0, use 'data', for 8 < version < 9, use prependData
-                prependData: `
-                  @import "@/assets/scss/main.scss";
-                  `
-            }
-        }
-    }
+    
 }
