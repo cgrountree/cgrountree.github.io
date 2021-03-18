89766abd04754795a89cbbb74b0a2cbb74e6cfc1
cgrountree <cgrountree@gmail.com>
2021-03-18 13:42:49 -0400

    update views and add bulma and sass loader

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,28 @@
 <template>
-  <h1>Cody Rountree</h1>
-  <div id="nav">
-      <router-link to="/">home</router-link> |
-      <router-link to="/projects">projects</router-link> |
-      <router-link to="/cv">cv</router-link> |
-      <router-link to="/contact">contact</router-link> 
+  <div class="header">
+    <h1 class="name">Cody Rountree</h1>
+    <nav>
+      <router-link class="link" to="/">home</router-link>
+      <router-link class="link" to="/projects">projects</router-link>
+      <router-link class="link" to="/cv">cv</router-link>
+      <router-link class="link" to="/contact">contact</router-link>
+    </nav>
   </div>
   <router-view />
 </template>
 
 <script>
-
 export default {
-  name: 'App',
-  components: {
-  }
-}
+  name: "App",
+  components: {},
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Noto Sans CJK SC", "Open Sans", sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #81ac91;
-}
-:root {
-    --background: #1d1d23;
-}
-body {
-    background-color: var(--background) !important;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,31 +1,22 @@
 <template>
-  <div class="base-container">
-    this is the home
+  <div class="content-container">
+    <p>
+      here is a wall of text about myself that will take up a significant amount
+      of space. i will have to think of many things to say, so it may be
+      difficult. we will see if i can come up with enough things to say or else
+      i will remove this page entirely or fill it with different information.
+    </p>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'home',
-  props: {
-  }
-}
+  name: "home",
+  props: {},
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  font-size: 12pt;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+
 </style>
